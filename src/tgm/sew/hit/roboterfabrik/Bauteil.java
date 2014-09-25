66e293cf87ba0f292bc06ad418a3856e62416628
TGM-polydor <spolydor@student.tgm.ac.at>
2014-09-25 18:59:15 +0200

    Erweiterung der Klasse Bauteil

--- src/tgm/sew/hit/roboterfabrik/Bauteil.java
+++ src/tgm/sew/hit/roboterfabrik/Bauteil.java
@@ -17,9 +17,29 @@ public class Bauteil {
 
 	/**
 	 *
+	 * @param typ gibt an, welche art dieser Bauteil ist. Gültige eingaben: "arm", "auge", "rumpf", "kettenl"
+	 * @param seriennr
+	 * @throws IllegalAccessException
+	 */
+	public Bauteil(String typ, int[] seriennr) throws IllegalAccessException {
+		if (typ.equals("arm"))
+			seriennr = new int[2];
+		else if (typ.equals("auge"))
+			seriennr = new int[2];
+		else if (typ.equals("rumpf"))
+			seriennr = new int[1];
+		else if (typ.equals("kettenl"))
+			seriennr = new int[1];
+		else throw new IllegalAccessException();
+		this.typ = typ;
+		this.seriennr = seriennr;
+	}
+
+	/**
+	 *
 	 * @return 1 wenn das Bauteil ein arm ist, 2 wenn es ein auge ist, 3 wenn es ein rumpf ist, 4 wenn es ein kettenl ist und 0 wenn es keines der vorher angeführten Typen ist
 	 */
-	public Integer isTyp() {
+	public int isTyp() {
 		switch (this.typ) {
 			case "arm":
 				return 1;
