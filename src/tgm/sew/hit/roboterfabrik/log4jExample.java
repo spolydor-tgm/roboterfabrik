4d362fd7b8845c35191b95402ab3e2ca059aa299
Hamberger Gregor <ghamberger@student.tgm.ac.at>
2014-09-28 15:05:15 +0200

    Implemention of writeLog(), to actually do something. Testing is pending.

--- src/tgm/sew/hit/roboterfabrik/log4jExample.java
+++ src/tgm/sew/hit/roboterfabrik/log4jExample.java
@@ -4,16 +4,23 @@ import org.apache.log4j.Logger;
 
 import java.io.*;
 import java.sql.SQLException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class log4jExample{
   /* Get actual class name to be printed on */
-  static Logger log = Logger.getLogger(
-                      log4jExample.class.getName());
+  static Logger log = Logger.getLogger(log4jExample.class.getName());
+  
+  public static void writeLog(String aufgabe) {
+	  DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+	  Date date = new Date();
+	  log.info(dateFormat.format(date) +" " +aufgabe);
+  }
 
   public static void main(String[] args)
                 throws IOException,SQLException{
    
-     log.info("Hello this is an info message");
+     writeLog("test123");
   }
 }
