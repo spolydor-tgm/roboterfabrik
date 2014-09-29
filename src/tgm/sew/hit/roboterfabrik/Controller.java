be9c6d3e3df781249422db9f369f1120f955b9fa
tgm-patrick-koc <cash1v@hotmail.com>
2014-09-29 14:35:24 +0200

    updated Controller

--- src/tgm/sew/hit/roboterfabrik/Controller.java
+++ src/tgm/sew/hit/roboterfabrik/Controller.java
@@ -1,5 +1,7 @@
 package tgm.sew.hit.roboterfabrik;
 
+import java.util.LinkedList;
+
 /**
  * Created by Patrick on 29.09.14.
  */
@@ -7,12 +9,18 @@ public class Controller {
 
 	public static void main(String[] args) {
 		CLI commandlineinterface = new CLI();
-		/*
-		 * logverzeichnis(commandlineinterface.getLager());
-		 *
-		 *
-		 */
+		LinkedList<Monteur> monteurLinkedList = new LinkedList<Monteur>();
+		int[] a = new int[6];
+		 //logverzeichnis(commandlineinterface.getLager());
+
 		Lagermitarbeiter lagermit = new Lagermitarbeiter(commandlineinterface.getLager());
+		Lieferant lieferant = new Lieferant(commandlineinterface.getLieferant());
+
+
+		for(int i = 0;i<commandlineinterface.getMonteur()+1;i++){
+			monteurLinkedList.add(new Monteur());
+			monteurLinkedList.start();
+		}
 
 	}
 
