d17236728421170436ed6bd51b81b68ef20db963
TGM-polydor <spolydor@student.tgm.ac.at>
2014-09-30 15:24:09 +0200

    Fix des Sekretariat's Hinzufuegen der ersten Testklassen

---
+++ src/tgm/sew/hit/roboterfabrik/MonteurTest.java
@@ -0,0 +1,26 @@
+package tgm.sew.hit.roboterfabrik;
+
+import org.junit.Test;
+
+public class MonteurTest {
+
+	@Test
+	public void testGetID() throws Exception {
+
+	}
+
+	@Test
+	public void testBauen() throws Exception {
+
+	}
+
+	@Test
+	public void testGetRoboter() throws Exception {
+
+	}
+
+	@Test
+	public void testSetBauteile() throws Exception {
+
+	}
+}
