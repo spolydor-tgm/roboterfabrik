80ff1b4259ff207d9f1881b4b23b312e7fe5fe84
tgm-patrick-koc <cash1v@hotmail.com>
2014-09-30 14:10:29 +0200

    added runner

---
+++ src/tgm/sew/hit/roboterfabrik/runner.java
@@ -0,0 +1,13 @@
+package tgm.sew.hit.roboterfabrik;
+
+/**
+ * Created by Patrick on 30.09.14.
+ */
+public class runner {
+
+
+	public static void main(String[] args) {
+
+		Controller a = new Controller();
+	}
+}
