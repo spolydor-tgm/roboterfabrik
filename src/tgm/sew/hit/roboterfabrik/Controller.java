cbcdbad7beee32fe43d9dc5664d6d4121a792e52
tgm-patrick-koc <cash1v@hotmail.com>
2014-10-01 16:31:46 +0200

    Controller updated,Lagermitarbeiter udated

--- src/tgm/sew/hit/roboterfabrik/Controller.java
+++ src/tgm/sew/hit/roboterfabrik/Controller.java
@@ -1,7 +1,8 @@
 package tgm.sew.hit.roboterfabrik;
 
+import org.apache.commons.cli.ParseException;
+
 import java.util.LinkedList;
-import org.apache.commons.cli.*;
 
 /**
  * Created by Patrick on 29.09.14.
@@ -20,40 +21,37 @@ public class Controller {
 
 		Lagermitarbeiter lagermit = new Lagermitarbeiter(Controller.getLagerVerzeichnis(args));
 		//Lieferant lieferant = new Lieferant(Controller.getLagerVerzeichnis(args));
-		System.out.print("schlafen start");
+
 		TimerWD timer= new TimerWD(Controller.getLaufzeit(args));
-		System.out.print("schlafen ende");
+
 
 
 		int[] ids = sekretariat.getUniqueIDs();
 
-		System.out.println(Controller.getLagerVerzeichnis(args));
-		System.out.println(Controller.getAnzahlLieferanten(args));
-		System.out.println(Controller.getAnzahlMonteure(args));
-		System.out.println(Controller.getLaufzeit(args));
+
 
 		//lieferant.liefern(Controller.getAnzahlLieferanten(args));
-		System.out.println("nach lieferant");
+
 		lagermit.readFile();
-		System.out.print("nachreadfile");
+
 		for(int i = 0;i<Controller.getAnzahlMonteure(args);i++){
-			System.out.print("ersteforschleife"+i);
+
 			monteurLinkedList.add(new Monteur(ids[i]));
 			monteurLinkedList.get(i).start();
 			monteurLinkedList.get(i).setBauteile(lagermit.getAlleBenoetigtenRoboterTeile());
 			monteurLinkedList.get(i).bauen(sekretariat.getId());
 		}
-		int k = 0;
+
 		do {
-			System.out.println(k);
-			k++;
+
+
 			//lieferant.liefern(Controller.getAnzahlLieferanten(args));
 			for (int i = 0; i < Controller.getAnzahlMonteure(args); i++) {
 				Roboter fertigrobo = monteurLinkedList.get(i).getRoboter();
 				lagermit.writeFile(fertigrobo);
 				monteurLinkedList.get(i).setBauteile(lagermit.getAlleBenoetigtenRoboterTeile());
 				monteurLinkedList.get(i).bauen(sekretariat.getId());
-				System.out.println("For schleife" + i);
+
 			}
 		}while( timer.tokeepRunning() == true);
 	}
